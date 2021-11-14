19781dd5e13d5096fd260cfc640d67463ae9f34c
Mohammed Alhadik <mohammed.s.alhadik@gmail.com>
2021-11-14 06:53:41 +0200

    Finshing Card Section - frontend

--- Frontend/src/app/property/card/card.component.ts
+++ Frontend/src/app/property/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-Card:any ={
+  @Input() property:any
+
+  Card:any ={
 "Id":1,
 "Title":"First Card Item",
 "Details":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius iusto praesentium est nisi, enim explicabo sunt eligendi magnam aperiam optio animi omnis suscipit mollitia quidem ut voluptate ipsa aspernatur minus.".substring(0,150)+" ..."
--- Frontend/src/app/property/property.component.ts
+++ Frontend/src/app/property/property.component.ts
@@ -6,7 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property.component.css']
 })
 export class PropertyComponent implements OnInit {
+  properties: Array<any> = [
+    {
+      "Id": 1,
+      "Title": "First Card Item",
+      "Details": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius iusto praesentium est nisi, enim explicabo sunt eligendi magnam aperiam optio animi omnis suscipit mollitia quidem ut voluptate ipsa aspernatur minus.".substring(0, 150) + " ..."
+    },
+    {
+      "Id": 2,
+      "Title": "Second Card Item",
+      "Details": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius iusto praesentium est nisi, enim explicabo sunt eligendi magnam aperiam optio animi omnis suscipit mollitia quidem ut voluptate ipsa aspernatur minus.".substring(0, 150) + " ..."
+    },{
+      "Id": 3,
+      "Title": "Third Card Item",
+      "Details": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius iusto praesentium est nisi, enim explicabo sunt eligendi magnam aperiam optio animi omnis suscipit mollitia quidem ut voluptate ipsa aspernatur minus.".substring(0, 150) + " ..."
+    },{
+      "Id": 4,
+      "Title": "Fourth Card Item",
+      "Details": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius iusto praesentium est nisi, enim explicabo sunt eligendi magnam aperiam optio animi omnis suscipit mollitia quidem ut voluptate ipsa aspernatur minus.".substring(0, 150) + " ..."
+    },{
+      "Id": 5,
+      "Title": "Fifth Card Item",
+      "Details": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius iusto praesentium est nisi, enim explicabo sunt eligendi magnam aperiam optio animi omnis suscipit mollitia quidem ut voluptate ipsa aspernatur minus.".substring(0, 150) + " ..."
+    },{
+      "Id": 6,
+      "Title": "sixth Card Item",
+      "Details": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius iusto praesentium est nisi, enim explicabo sunt eligendi magnam aperiam optio animi omnis suscipit mollitia quidem ut voluptate ipsa aspernatur minus.".substring(0, 150) + " ..."
+    },{
+      "Id": 7,
+      "Title": "seventh Card Item",
+      "Details": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius iusto praesentium est nisi, enim explicabo sunt eligendi magnam aperiam optio animi omnis suscipit mollitia quidem ut voluptate ipsa aspernatur minus.".substring(0, 150) + " ..."
+    }
 
+  ]
   constructor() { }
 
   ngOnInit(): void {
